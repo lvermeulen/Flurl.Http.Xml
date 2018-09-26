85fd93d59c007fe81edbb2f7067491f643bd21ba
Luk Vermeulen <luk.vermeulen@gmail.com>
2018-09-26 13:43:45 +0200

    use Flurl.Http 2.4.0

--- test/Flurl.Http.Xml.Tests/MicrosoftXmlSerializerShould.cs
+++ test/Flurl.Http.Xml.Tests/MicrosoftXmlSerializerShould.cs
@@ -3,6 +3,8 @@ using System.Xml;
 using Flurl.Http.Xml.Tests.Models;
 using Xunit;
 
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
 namespace Flurl.Http.Xml.Tests
 {
     public class MicrosoftXmlSerializerShould
